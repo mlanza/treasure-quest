6c437704cda01e7188d3946233adde313d01d128
Mario T. Lanza <me@mlanza.com>
2022-07-15 20:50:44 -0400

    refine update model

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 import _ from "./lib/@atomic/core.js";
 import dom from "./lib/@atomic/dom.js";
+import t from "./lib/@atomic/transducers.js";
 import $ from "./lib/@atomic/reactives.js";
 import * as cmd from "./lib/cmd.js";
 import * as q from "./lib/treasure-quest.js";
 
 const $state = $.cell(q.init());
+const $hist = $.hist($state);
 const el = dom.sel1("#quest");
-const td = dom.tag('td');
-const tr = dom.tag('tr');
-const tbody = dom.tag('tbody');
+const span = dom.tag('span');
 
-$.sub($state, function(rows){
-  _.chain(rows, _.mapa(_.comp(tr, function(row){
-    return _.mapa(function(key){
-      return td(key ? {"data-piece": key} : null, q.icon(key));
-    }, row);
-  }), _), dom.html(el, _));
+const $keys = $.chan(document, "keydown");
+const $move = $.pipe($keys, _.comp(t.map(function(e){
+  return e.key;
+}), t.filter(_.startsWith(_, "Arrow")), t.map(_.lowerCase), t.map(_.replace(_, "arrow", ""))));
+$.sub($move, function(move){
+  _.swap($state, q.move(_, move));
+});
+$.sub($state, _.log);
+$.sub($hist, function([curr, prior]){
+  if (prior) {
+
+  } else {
+    const els = _.mapkv(function(piece, vals){
+      const icon = q.icon(piece);
+      return _.mapa(function([x, y]){
+        return span({"data-piece": piece, "data-x": x, "data-y": y}, icon);
+      }, vals);
+    }, curr);
+    dom.html(el, els);
+  }
 });
 
